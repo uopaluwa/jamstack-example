a26f76749b329972abdaa23fd47ead8780e88ab2
uopaluwa <ufedojo.opaluwa@gmail.com>
2019-10-10 13:23:59 +0100

    form submission

--- src/components/AppForm.vue
+++ src/components/AppForm.vue
@@ -1,9 +1,12 @@
 <template>
   <div>
-    <form name="contact" method="POST">
+    <form name="success" method="POST" data-netlify="true" netlify-honeypot="bot-field">
       <!--add some things here to make it work with Netlify-->
       <p class="visually-hidden">
         <!--add something here to catch the bots!-->
+        <label> Don't fill this if you're human
+          <input name="bot-field" />
+        </label>
       </p>
       <p>
         <label>
---
+++ src/views/Sucess.vue
@@ -0,0 +1,17 @@
+<template>
+  <main>
+    <h1>Your message has been sent. We'll be in touch 😉</h1>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  width: 90%;
+  max-width: 800px;
+  margin: 40px auto;
+}
+
+p {
+  text-align: center;
+}
+</style>
