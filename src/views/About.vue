f37f80fb6c02317f3e479c22819eebf2a01a0ad5
uopaluwa <ufedojo.opaluwa@gmail.com>
2019-10-10 12:39:29 +0100

    initial commit

---
+++ src/views/About.vue
@@ -0,0 +1,18 @@
+<template>
+  <main>
+    <h1>Hey there</h1>
+    <p>I'm Ufedo, a software developer by day and a crime fighting superhero at night in my dreams.</p>
+  </main>
+</template>
+
+<style lang="scss" scoped>
+main {
+  width: 90%;
+  max-width: 800px;
+  margin: 40px auto;
+}
+
+p {
+  text-align: center;
+}
+</style>
